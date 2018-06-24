608a2111098a8f0448c6ba0c64bb3c0b587254c3
Florian Wittmann <dev@florianwittmann.de>
2018-06-24 20:51:30 +0200

    Erstellen und Einbinden der Eintragskomponente

---
+++ src/Eintrag.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class Eintrag extends Component {
+    render() {
+        return (<li>Eintrag</li>);
+    }
+}
+
+export default Eintrag;
--- src/Liste.js
+++ src/Liste.js
@@ -1,9 +1,15 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import Eintrag from './Eintrag';
 
 class Liste extends Component {
 
     render() {
-        return (<div><p>Leere Liste</p></div>)
+        return (<div><ol>
+            <Eintrag />
+            <Eintrag />
+            <Eintrag />
+            <Eintrag />
+        </ol></div>)
     }
 }
 
